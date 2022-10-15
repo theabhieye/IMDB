f7655d122ed604c2868b57237ec96c1f38f7f1f3
Abhishek Kapoor <abhishek.kapoor@myparkplus.com>
2022-10-15 14:11:41 +0530

    Initial Commit

---
+++ IMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMDB
+//
+//  Created by Abhishek kapoor on 15/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
